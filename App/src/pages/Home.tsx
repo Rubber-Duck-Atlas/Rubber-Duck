e18114c45d11419717bb1db4bc33fe2bf6254171
Jerry Todd <jerrytodd2005@gmail.com>
2026-07-17 14:44:38 -0500

    rubber duck concept output

--- App/src/pages/Home.tsx
+++ App/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import TextFade from '../components/TextFade';
 import { Folder } from 'lucide-react';
 import { useNavigate } from "react-router-dom";
@@ -7,14 +7,16 @@ import { useNavigate } from "react-router-dom";
 
 export default function Home() {
     const navigate = useNavigate();
+    const [query, setQuery] = useState<string | undefined>(undefined);
+    const [searchView, setSearchView] = useState<boolean>(false);
 
     const text = "What can I find for you?"
 
     return (<>
-        <main className="min-h-screen bg-ink text-peri flex flex-col gap-4 justify-center items-center">
+        <main className={`min-h-screen bg-ink text-peri flex flex-col p-4 gap-4 justify-${searchView ? 'start' : 'center'} items-center`}>
 
             {/* Fade in animation per character */}
-            <TextFade className='font-bold text-3xl cursor-default' text={text} /> 
+            <TextFade className={`${searchView ? 'hidden ' : ''}font-bold text-3xl cursor-default animate-duration`} text={text} /> 
 
             <input type="text" name="search" id="search" placeholder='Search...'
                 className='p-2 w-96 rounded-xl bg-peri text-ink font-bold animate-fade-up opacity-0' style={{
@@ -26,6 +28,7 @@ export default function Home() {
 
         {/* Link to prototype file viewer thingy */}
         <Folder className='absolute top-3 left-3 w-8 h-8 text-white cursor-pointer' onClick={() => navigate("/Documents")} />
+        <div className='absolute bottom-3 left-3 p-1 bg-white cursor-pointer' onClick={() => setSearchView(!searchView)} >debug</div>
     </>
     );
 }
