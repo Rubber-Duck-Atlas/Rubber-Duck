d5a0182ae8c6e5a352d96a3ec03bbbfeae528110
ryliei <144707791+ryliei@users.noreply.github.com>
2026-07-20 11:39:22 -0500

    added auth layout and skeleton welcome page. about to pull from main

---
+++ App/src/layouts/AuthLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+// gives onboarding and authentication pages a consistent full-screen layout
+export default function AuthLayout() {
+  return (
+    // Gives a full screen height and puts everything in a central column layout with padding
+    <div className="min-h-screen flex flex-col items-center justify-center p-6">
+      {/* Displays current page */}
+      <Outlet />
+    </div>
+  );
+}
