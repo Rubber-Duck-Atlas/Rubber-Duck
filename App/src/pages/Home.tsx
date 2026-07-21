a6c2440686c8aa19e763ea92efe000ab1ec8ac6c
Jerry Todd <jerrytodd2005@gmail.com>
2026-07-21 12:27:55 -0500

    Wired up rubber ducky somewhat, just prints results to console for now and parses results with regex

--- App/src/pages/Home.tsx
+++ App/src/pages/Home.tsx
@@ -1,34 +1,67 @@
-
-import React, { useState } from 'react';
-import TextFade from '../components/TextFade';
-import { Folder } from 'lucide-react';
+import React, { useState } from "react";
+import TextFade from "../components/TextFade";
+import { Folder } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-
+import RubberDuck from "../lib/RubberDuck";
 
 export default function Home() {
-    const navigate = useNavigate();
-    const [query, setQuery] = useState<string | undefined>(undefined);
-    const [searchView, setSearchView] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const [query, setQuery] = useState<string>("");
+  const [searchView, setSearchView] = useState<boolean>(false);
 
-    const text = "What can I find for you?"
+  const text = "What can I find for you?";
 
-    return (<>
-        <main className={`min-h-screen bg-ink text-peri flex flex-col p-4 gap-4 justify-${searchView ? 'start' : 'center'} items-center`}>
+  const handleQuery = () => {
+    if (!query) return;
+    RubberDuck(query).then((result) => {
+      console.log(result);
+    });
+  };
 
-            {/* Fade in animation per character */}
-            <TextFade className={`${searchView ? 'hidden ' : ''}font-bold text-3xl cursor-default animate-duration`} text={text} /> 
+  const handleSearchKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleQuery();
+    }
+  };
 
-            <input type="text" name="search" id="search" placeholder='Search...'
-                className='p-2 w-96 rounded-xl bg-peri text-ink font-bold animate-fade-up opacity-0' style={{
-                    animationDelay: `${text.length * 10 - 100}ms`,
-                    animationFillMode: "forwards",
-                }} />
+  return (
+    <>
+      <main
+        className={`min-h-screen bg-ink text-peri flex flex-col p-4 gap-4 justify-${searchView ? "start" : "center"} items-center`}
+      >
+        {/* Fade in animation per character */}
+        <TextFade
+          className={`${searchView ? "hidden " : ""}font-bold text-3xl cursor-default animate-duration`}
+          text={text}
+        />
 
-        </main>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search..."
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          onKeyDown={handleSearchKeyDown}
+          className="p-2 w-96 rounded-xl bg-peri text-ink font-bold animate-fade-up opacity-0"
+          style={{
+            animationDelay: `${text.length * 10 - 100}ms`,
+            animationFillMode: "forwards",
+          }}
+        />
+      </main>
 
-        {/* Link to prototype file viewer thingy */}
-        <Folder className='absolute top-3 left-3 w-8 h-8 text-white cursor-pointer' onClick={() => navigate("/Documents")} />
-        <div className='absolute bottom-3 left-3 p-1 bg-white cursor-pointer' onClick={() => setSearchView(!searchView)} >debug</div>
+      {/* Link to prototype file viewer thingy */}
+      <Folder
+        className="absolute top-3 left-3 w-8 h-8 text-white cursor-pointer"
+        onClick={() => navigate("/Documents")}
+      />
+      <div
+        className="absolute bottom-3 left-3 p-1 bg-white cursor-pointer"
+        onClick={() => setSearchView(!searchView)}
+      >
+        debug
+      </div>
     </>
-    );
+  );
 }
