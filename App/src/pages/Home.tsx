fe0e8ad7ecd3dc7c42730b203a9dcd4e82198b09
Jerry Todd <jerrytodd2005@gmail.com>
2026-08-03 09:26:58 -0500

    Add section

--- App/src/pages/Home.tsx
+++ App/src/pages/Home.tsx
@@ -11,33 +11,43 @@ export default function Home() {
   const [lastSearch, setLastSearch] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [searchView, setSearchView] = useState<boolean>(false);
-  const [queriedDocuments, setQueriedDocuments] = useState<RubberDuckResult | undefined>(undefined);
-  const [queriedNotes, setQueriedNotes] = useState<RubberDuckResult | undefined>(undefined);
+  const [queriedDocuments, setQueriedDocuments] = useState<
+    RubberDuckResult | undefined
+  >(undefined);
+  const [queriedNotes, setQueriedNotes] = useState<
+    RubberDuckResult | undefined
+  >(undefined);
 
   const text = "What can I find for you?";
 
   const handleQuery = () => {
-    setLoading(true)
+    setLoading(true);
     if (!query) return;
     RubberDuck(query, false).then((document_results) => {
       RubberDuck(query, true).then((note_results) => {
-        setLastSearch(query)
-        setQueriedDocuments(document_results)
-        setQueriedNotes(note_results)
-        setSearchView(true)
-        setLoading(false)
+        setLastSearch(query);
+        setQueriedDocuments(document_results);
+        setQueriedNotes(note_results);
+        setSearchView(true);
+        setLoading(false);
       });
     });
   };
 
-  const handleSearchKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleSearchKeyDown = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ) => {
     if (event.key === "Enter") {
       handleQuery();
     }
   };
 
-  const sortedDocuments = [...(queriedDocuments?.results ?? [])].sort((a, b) => b.score - a.score);
-  const sortedNotes = [...(queriedNotes?.results ?? [])].sort((a, b) => b.score - a.score);
+  const sortedDocuments = [...(queriedDocuments?.results ?? [])].sort(
+    (a, b) => b.score - a.score,
+  );
+  const sortedNotes = [...(queriedNotes?.results ?? [])].sort(
+    (a, b) => b.score - a.score,
+  );
 
   return (
     <>
@@ -45,41 +55,67 @@ export default function Home() {
         className={`min-h-screen bg-ink text-peri flex flex-col p-4 gap-4 justify-${searchView ? "start" : "center"} items-center`}
       >
         {/* Fade in animation per character */}
-        <TextFade
-          className={`${searchView ? "hidden " : ""}font-bold text-3xl cursor-default animate-duration`}
-          text={text}
-        />
-        <div className="p-2 gap-2 w-96 rounded-xl bg-peri text-ink font-bold animate-fade-up opacity-0 flex justify-between">
-          <input
-            type="text"
-            name="search"
-            id="search"
-            placeholder="Search..."
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-            onKeyDown={handleSearchKeyDown}
-            className="flex-1"
-            style={{
-              animationDelay: `${text.length * 10 - 100}ms`,
-              animationFillMode: "forwards",
-            }}
-          />{loading ? <LoaderCircle className="animate-spin" /> : <Forward onClick={handleQuery} />}
-        </div>
-        {searchView ? <div className="flex flex-col gap-4">
-          <h1 className="font-bold text-lg text-center flex mx-auto">
-            Showing results for "{lastSearch}" <X className="m-auto cursor-pointer text-red-400" onClick={() => {
-              setSearchView(false)
-            }}/>
-          </h1> 
-          <div className="flex gap-4">
-            <SearchResultsList title="Documents" results={sortedDocuments} isNote={false}/>
-            <SearchResultsList title="Notes" results={sortedNotes} isNote={true}/>
+        <div className={`flex-1 ${searchView ? 'hidden' : ''}`}></div>
+        <div>
+          <TextFade
+            className={`${searchView ? "hidden " : ""}font-bold text-3xl cursor-default animate-duration mb-4 w-full text-center`}
+            text={text}
+          />
+          <div className="p-2 gap-2 w-96 rounded-xl bg-peri text-ink font-bold animate-fade-up opacity-0 flex justify-between">
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search..."
+              value={query}
+              onChange={(event) => setQuery(event.target.value)}
+              onKeyDown={handleSearchKeyDown}
+              className="flex-1"
+              style={{
+                animationDelay: `${text.length * 10 - 100}ms`,
+                animationFillMode: "forwards",
+              }}
+            />
+            {loading ? (
+              <LoaderCircle className="animate-spin" />
+            ) : (
+              <Forward onClick={handleQuery} />
+            )}
           </div>
-        </div> : (
-          <></>
+        </div>
+        {searchView ? (
+          <div className="flex flex-col gap-4">
+            <h1 className="font-bold text-lg text-center flex mx-auto">
+              Showing results for "{lastSearch}"{" "}
+              <X
+                className="m-auto cursor-pointer text-red-400"
+                onClick={() => {
+                  setSearchView(false);
+                }}
+              />
+            </h1>
+            <div className="flex gap-4">
+              <SearchResultsList
+                title="Documents"
+                results={sortedDocuments}
+                isNote={false}
+              />
+              <SearchResultsList
+                title="Notes"
+                results={sortedNotes}
+                isNote={true}
+              />
+            </div>
+          </div>
+        ) : (
+          <>
+          {/*  */}
+          <div className="flex-1 text-peri">
+            Recent searches
+          </div>
+          </>
         )}
       </main>
-
 
       {/* Link to prototype file viewer thingy */}
       <Folder
